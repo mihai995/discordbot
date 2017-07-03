40bf8e35327227f91f3ba50e50c0842972f63c41
mihai995 <mandreescu@uchicago.edu>
2017-07-03 17:00:38 -0500

    removed memes

--- memes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.5
-
-import asyncio
-
-async def postmemes(bot, msg):
-  await self.reply(msg, 'functionality not yet implemented')
-
-async def approve(bot, msg):
-  await self.reply(msg, 'functionality not yet implemented')
